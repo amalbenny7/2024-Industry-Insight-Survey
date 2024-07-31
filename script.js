e7665486372e26717cc2ce847d06bc47940bee9f
Amal Benny <57700315+amalbenny7@users.noreply.github.com>
2024-07-31 15:03:38 +0100

    Add files via upload
    Initial and final commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('surveyButton').addEventListener('click', function() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSf4v3i4hqdCNya5g2_xeJ_w7DvTYSm064zGG6VftCas7qtMjg/viewform'; // Actual link to the survey form
+});
